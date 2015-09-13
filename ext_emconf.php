2b573ec23ba8d434f481ec695ed58a0bac206db7
Arek van Schaijk <info@ucreation.nl>
2015-09-13 16:26:00 +0200

    Increase version number Relates-to: JCC-1

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,20 +1,10 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "jcc_appointments".
- *
- * Auto generated 05-06-2015 13:23
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
 $EM_CONF[$_EXTKEY] = array (
 	'title' => 'JCC Appointment Module',
 	'description' => 'This extension provides a Appointment module based on the JCC Software API',
 	'category' => 'plugin',
-	'version' => '2.4.0',
+	'version' => '2.5.0',
 	'state' => 'stable',
 	'uploadfolder' => false,
 	'createDirs' => '',
